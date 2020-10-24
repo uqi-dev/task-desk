99ee0b2eb13edf151387b3e42a44a656bce024ab
desyed <syedshihabdu@gmail.com>
2020-10-24 19:14:41 +0600

    check git

--- src/main/java/com/uqidev/taskdesk/controller/TestController.java
+++ src/main/java/com/uqidev/taskdesk/controller/TestController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+
     @RequestMapping(value = "/")
     public String test() {
         return "jmhcvc";
